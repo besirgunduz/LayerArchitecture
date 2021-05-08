29efca8bc767320c6c7c4314d418987ac27ac553
Besir Gunduz <besirgunduz1993@gmail.com>
2021-05-09 02:50:53 +0300

    09.05.2020 Commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -31,7 +31,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 03)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
