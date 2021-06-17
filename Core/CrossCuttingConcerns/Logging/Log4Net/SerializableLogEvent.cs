7aa30bf0e8a2be749830be832c9b9b1c7957012b
Besir Gunduz <besirgunduz1993@gmail.com>
2021-06-17 23:52:17 +0300

    17.06.2021 Commit

---
+++ Core/CrossCuttingConcerns/Logging/Log4Net/SerializableLogEvent.cs
@@ -0,0 +1,20 @@
+﻿using log4net.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Logging.Log4Net
+{
+    [Serializable]
+   public class SerializableLogEvent
+    {
+        //loglayacağımız data
+
+        LoggingEvent _loggingEvent;
+
+        public SerializableLogEvent(LoggingEvent loggingEvent)
+        {
+            _loggingEvent = loggingEvent;
+        }
+    }
+}
